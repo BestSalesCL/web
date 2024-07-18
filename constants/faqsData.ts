573f28af5722195338d73ea0d185789e355a1166
BestLevel <reachbestlevel@gmail.com>
2024-07-18 17:38:26 -0400

    si

--- constants/faqsData.ts
+++ constants/faqsData.ts
@@ -6,55 +6,69 @@ type Faqs = {
 
 export const FaqsData: Faqs[] = [
   {
-    title: "01 - Is the BestLevel app compatible with my devices?",
+    title: "01 - ¿Qué servicios de marketing ofrecemos?",
     content:
-      "It is designed to be used with your cell phone, can be used on a computer, and is not compatible with tablets at the moment. It is available on iOS and Android.",
+      "Ofrecemos una variedad de servicios, incluyendo Desarrollo Web, Creación de Contenido, Manejo de Campañas y Asesoría de Marketing.",
     value: "question-1",
   },
   {
-    title: "02 - Do I need to pay every month?",
+    title: "02 - ¿Cómo pueden ayudarme a aumentar mis ventas?",
     content:
-      "Yes, it is a service that is paid every month. This is perfect to test whether or not this modality works for you, since you can stop paying whenever you want. No long-term commitments",
+      "Utilizamos estrategias personalizadas basadas en los datos obtenidos de nuestra investigación de la competencia para atraer a tu público objetivo, mejorar la conversión y fidelizar a los clientes existentes.",
     value: "question-2",
   },
   {
-    title: "03 - How does the personal training aspect work online?",
+    title: "03 - ¿Cuál es su proceso de trabajo?",
     content:
-      "The only thing you do “alone” is the workout, but we will be there to support and motivate you to smash your results. We will have constant feedback and ensure you are correctly following the plan. You can talk with your trainer in the app.",
+      "Nuestro proceso incluye Investigación de la Competencia, planificación estratégica, implementación de campañas, monitoreo y optimización continua.",
     value: "question-3",
   },
   {
-    title: "04 - Do I need a Gym?",
+    title: "04 - ¿Cuánto tiempo tomará ver resultados?",
     content:
-      "No, Best Level will adapt to your needs to provide you with the optimal planning to achieve your objectives. There are no excuses not to train. However, if you have access to a gym, this can further enhance your training experience.",
+      "El tiempo puede variar según el tipo de campaña y los objetivos, pero generalmente se pueden observar resultados en un periodo de 5-7 días.",
     value: "question-4",
   },
   {
     title:
-      "05 - Do the trainings adapt to the competitions, championships, and matches that I have?",
+      "05 - ¿Qué información necesitan de mi parte para empezar?",
     content:
-      "Of course, your competition schedule is a crucial element for your coach to consider in order to plan your training and ensure you achieve the best possible performance.",
+      "Necesitamos información sobre tus objetivos de negocio, tu público objetivo, tus competidores, y acceso a tus cuentas de redes sociales y sitio web.",
     value: "question-5",
   },
   {
     title:
-      "06 - If I play a team sport, does BestLevel take my position into account?",
+      "06 - ¿Puedo cancelar los servicios en cualquier momento?",
     content:
-      "At BestLevel, we are known for our personalized training. We take into account your position and all aspects of your life and performance that will help us get to know you better and achieve good results.",
+      "Sí, puedes cancelar los servicios, pero recomendamos cumplir con el período mínimo acordado para ver resultados significativos.",
     value: "question-6",
   },
   {
     title:
-      "07 - How does online training integrate with my current sports training?",
+      "07 - ¿Cómo se mide el éxito de las campañas?",
     content:
-      "Our online training programs are designed to complement your current sports training. We will collaborate with you to effectively integrate the online training program into your existing training routine, ensuring that both work in harmony to help you achieve your athletic goals.",
+      "Utilizamos métricas clave dependiendo de tus objetivos, como por ejemplo: ROAS, conversaciones iniciadas, conversiones, entre otras.",
     value: "question-7",
   },
   {
     title:
-      "08 - How does the online training program adapt to my specific needs as an athlete?",
+      "08 - ¿Cómo garantizan la calidad de su trabajo?",
     content:
-      "Our coaches customize each online training program based on your specific needs as an athlete, including your sport, skill level, personal goals, previous injuries, and any other relevant considerations. This ensures that the program is fully tailored to you and your athletic goals.",
+      "Tenemos una política que garantiza que, si luego de 30 días de la contratación de nuestros servicios se demuestra que se aplicaron los conceptos aplicados pero las ventas de las campañas de publicidad en Facebook e Instagram no presentan incrementos, se devolverá el 100% del dinero.",
     value: "question-8",
+  },
+  {
+    title:
+      "09 - ¿Cuál es el costo de sus servicios?",
+    content:
+      "Los costos varían según el presupuesto invertido, generalmente entre un 5% y un 20%.",
+    value: "question-9",
+  },
+  {
+    title:
+      "10 - ¿Qué tipo de empresas han trabajado con ustedes?",
+    content:
+      "Hemos trabajado con una variedad de empresas, desde startups hasta grandes corporaciones, en sectores como retail, tecnología, salud, finanzas e incluso marcas personales.",
+    value: "question-10",
   },
 ];
