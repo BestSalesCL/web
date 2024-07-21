847c93a267cc9e8d441d7ba81cc867f61e6e6514
BestLevel <reachbestlevel@gmail.com>
2024-07-21 03:04:30 -0400

    s

---
+++ pages/_app.tsx
@@ -0,0 +1,32 @@
+// pages/_app.tsx
+
+import { AppProps } from 'next/app';
+import Script from 'next/script';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Script
+        id="facebook-pixel"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f.fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '${process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID}');
+            fbq('track', 'PageView');
+          `,
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
