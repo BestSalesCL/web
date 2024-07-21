8fe3fe7d68db4df430b8581142da7a4a9919d57d
BestLevel <reachbestlevel@gmail.com>
2024-07-21 01:43:09 -0400

    s

--- app/_app.tsx
+++ app/_app.tsx
@@ -1,4 +1,3 @@
-// app/_app.tsx
 import { AppProps } from 'next/app';
 import Script from 'next/script';
 import { useEffect } from 'react';
@@ -6,15 +5,27 @@ import { useEffect } from 'react';
 function MyApp({ Component, pageProps }: AppProps) {
   useEffect(() => {
     // Facebook Pixel code initialization
-    if (typeof window !== 'undefined' && !window.fbq) {
-      !function(f, b, e, v, n, t, s) {
-        if (f.fbq) return; n = f.fbq = function() { n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments) };
-        if (!f._fbq) f._fbq = n; n.push = n; n.loaded = !0; n.version = '2.0'; n.queue = [];
-        t = b.createElement(e); t.async = !0; t.src = v; s = b.getElementsByTagName(e)[0];
-        s.parentNode.insertBefore(t, s)
-      }(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');
-      fbq('init', process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID);
-      fbq('track', 'PageView');
+    if (typeof window !== 'undefined') {
+      (function(f, b, e, v, n, t, s) {
+        if (f.fbq) return;
+        n = f.fbq = function() {
+          n.callMethod
+            ? n.callMethod.apply(n, arguments)
+            : n.queue.push(arguments);
+        };
+        if (!f._fbq) f._fbq = n;
+        n.push = n;
+        n.loaded = !0;
+        n.version = '2.0';
+        n.queue = [];
+        t = b.createElement(e);
+        t.async = !0;
+        t.src = v;
+        s = b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t, s);
+      })(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');
+      window.fbq('init', process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID);
+      window.fbq('track', 'PageView');
     }
   }, []);
 
